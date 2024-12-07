72eda7674b1e87d08216681b9aa0a32ac4c9cc4c
chn038 <chenhero940830@gmail.com>
2024-12-07 20:42:34 +0800

    rofi

---
+++ lxqt/qtile/autostart.sh
@@ -0,0 +1,9 @@
+blueman-applet &
+fcitx5 &
+firewall-applet &
+kdeconnectd &
+kdeconnect-indicator &
+system-config-printer-applet &
+gnome-keyring-daemon --start --components=ssh &
+copyq &
+nm-applet &
