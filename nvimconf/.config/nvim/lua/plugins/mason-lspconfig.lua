bcae56d2f78cb41d7ae2454e953e6425db65dc16
chn038 <chenhero940830@gmail.com>
2025-05-05 18:51:45 +0800

    add linters

--- nvimconf/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvimconf/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -47,5 +47,30 @@ return {
         config = function()
             require("dapui").setup()
         end
+    },
+    {
+        "rshkarin/mason-nvim-lint",
+        dependencies = {
+            "mfussenegger/nvim-lint",
+        },
+        config = function()
+            local DEFAULT_SETTINGS = {
+                -- A list of linters to automatically install if they're not already installed. Example: { "eslint_d", "revive" }
+                -- This setting has no relation with the `automatic_installation` setting.
+                -- Names of linters should be taken from the mason's registry.
+                ---@type string[]
+                ensure_installed = {},
+
+                -- Whether linters that are set up (via nvim-lint) should be automatically installed if they're not already installed.
+                -- It tries to find the specified linters in the mason's registry to proceed with installation.
+                -- This setting has no relation with the `ensure_installed` setting.
+                ---@type boolean
+                automatic_installation = false,
+
+                -- Disables warning notifications about misconfigurations such as invalid linter entries and incorrect plugin load order.
+                quiet_mode = false,
+            }
+            require('mason-nvim-lint').setup(DEFAULT_SETTINGS)
+        end
     }
 }
