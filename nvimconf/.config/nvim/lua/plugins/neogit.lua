dc0945ed86266fdfdf3926d0b874e4dd0f8975e6
chn038 <chenhero940830@gmail.com>
2025-05-10 17:17:15 +0800

    remove mini diff

--- nvimconf/.config/nvim/lua/plugins/neogit.lua
+++ nvimconf/.config/nvim/lua/plugins/neogit.lua
@@ -1,12 +1,5 @@
 return {
     {
-        'echasnovski/mini.diff',
-        version = false,
-        config = function()
-            require("mini.diff").setup()
-        end
-    },
-    {
         "NeogitOrg/neogit",
         dependencies = {
             "nvim-lua/plenary.nvim",         -- required
