790f2823c5b4c86cbb5384c49032b9b3a5930c77
chn038 <chenhero940830@gmail.com>
2025-05-11 19:31:13 +0800

    add some useful keybinding with lsp, loclist, qflist

--- nvimconf/.config/nvim/lua/config/post.lua
+++ nvimconf/.config/nvim/lua/config/post.lua
@@ -3,6 +3,24 @@ vim.cmd("colorscheme rose-pine")
 -- leader key fall back
 vim.keymap.set({'n', 'v'}, "<leader><leader>", " ", { desc = "just insert the space" })
 
+-- general with lsp
+--'omnifunc' is set to vim.lsp.omnifunc(), use i_CTRL-X_CTRL-O to trigger completion.
+-- 'tagfunc' is set to vim.lsp.tagfunc(). This enables features like go-to-definition, :tjump, and keymaps like CTRL-], CTRL-W_], CTRL-W_} to utilize the language server.
+-- 'formatexpr' is set to vim.lsp.formatexpr(), so you can format lines via gq if the language server supports it.
+-- To opt out of this use gw instead of gq, or clear 'formatexpr' on LspAttach.
+-- K is mapped to vim.lsp.buf.hover() unless 'keywordprg' is customized or a custom keymap for K exists.
+-- GLOBAL DEFAULTS
+-- grr gra grn gri i_CTRL-S These GLOBAL keymaps are created unconditionally when Nvim starts:
+-- "grn" is mapped in Normal mode to vim.lsp.buf.rename()
+-- "gra" is mapped in Normal and Visual mode to vim.lsp.buf.code_action()
+-- "grr" is mapped in Normal mode to vim.lsp.buf.references()
+-- "gri" is mapped in Normal mode to vim.lsp.buf.implementation()
+-- "gO" is mapped in Normal mode to vim.lsp.buf.document_symbol()
+-- CTRL-S is mapped in Insert mode to vim.lsp.buf.signature_help()
+vim.keymap.set({'n'}, '<leader>h', vim.diagnostic.open_float, { desc = "open diagnostic in float window" })
+vim.keymap.set({'n'}, '<leader>q', function() vim.diagnostic.setqflist(vim.diagnostic.get_namespaces()) end, { desc = "move all diagnostic to quick fix list" })
+vim.keymap.set({'n'}, '<leader>l', function() vim.diagnostic.setloclist(vim.diagnostic.get(0)) end, { desc = "move all diagnostic to location list" })
+
 -- deal with file
 vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files hidden=true<cr>', { desc = 'Open fzf file finder'})
 vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua git_files hidden=true<cr>', { desc = 'Open fzf git file finder'})
@@ -27,5 +45,3 @@ vim.keymap.set({'n', 'v'}, "<leader>de", function() require("dapui").eval() end,
 
 -- deal with git
 vim.keymap.set('n', '<leader>gg', "<cmd>Neogit<CR>", { desc = "Open Neogit" })
-
--- deal with ai, start with ta
