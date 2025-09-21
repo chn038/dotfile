3419a9accccd0479206310f363103cd300a84883
chn038 <chenhero940830@gmail.com>
2025-09-22 02:48:33 +0800

    use wezterm to display image

--- nvimconf/.config/nvim/lua/plugins/iron.lua
+++ nvimconf/.config/nvim/lua/plugins/iron.lua
@@ -6,21 +6,12 @@ return {
         build = ":UpdateRemotePlugins",
         init = function()
             -- these are examples, not defaults. Please see the readme
-            vim.g.molten_image_provider = "image.nvim"
+            vim.g.molten_image_provider = "wezterm"
             vim.g.molten_output_win_max_height = 20
         end,
     },
     {
-        -- see the image.nvim readme for more information about configuring this plugin
-        "3rd/image.nvim",
-        opts = {
-            backend = "ueberzug", -- whatever backend you would like to use
-            max_width = 100,
-            max_height = 12,
-            max_height_window_percentage = math.huge,
-            max_width_window_percentage = math.huge,
-            window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
-            window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        },
-    },
+        'willothy/wezterm.nvim',
+        config = true
+    }
 }
