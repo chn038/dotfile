34b340cc0d2dad85077c369db8d951410bfc1380
chn038 <chenhero940830@gmail.com>
2025-09-22 16:29:09 +0800

    fix

--- nvimconf/.config/nvim/lua/plugins/iron.lua
+++ nvimconf/.config/nvim/lua/plugins/iron.lua
@@ -5,6 +5,7 @@ return {
         build = ":UpdateRemotePlugins",
         init = function()
             -- these are examples, not defaults. Please see the readme
+            vim.g.molten_auto_open_output = false
             vim.g.molten_image_provider = "wezterm"
             vim.g.molten_output_win_max_height = 20
         end,
