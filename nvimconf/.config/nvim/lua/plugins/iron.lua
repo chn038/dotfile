bbbb1248061cff3b68d028a7928bd4dc8d6b238b
chn038 <chenhero940830@gmail.com>
2025-09-29 22:21:28 +0800

    add some config to molten to try to fix the image problem

--- nvimconf/.config/nvim/lua/plugins/iron.lua
+++ nvimconf/.config/nvim/lua/plugins/iron.lua
@@ -1,17 +1,24 @@
 return {
     {
+        'willothy/wezterm.nvim',
+        config = true
+    },
+    {
         "benlubas/molten-nvim",
         version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
         build = ":UpdateRemotePlugins",
         init = function()
-            -- these are examples, not defaults. Please see the readme
-            vim.g.molten_auto_open_output = false
+            vim.g.molten_auto_open_output = false -- cannot be true if molten_image_provider = "wezterm"
+            vim.g.molten_output_show_more = true
             vim.g.molten_image_provider = "wezterm"
-            vim.g.molten_output_win_max_height = 20
+            vim.g.molten_output_virt_lines = true
+            vim.g.molten_split_direction = "right" --direction of the output window, options are "right", "left", "top", "bottom"
+            vim.g.molten_split_size = 40 --(0-100) % size of the screen dedicated to the output window
+            vim.g.molten_virt_text_output = true
+            vim.g.molten_use_border_highlights = true
+            vim.g.molten_virt_lines_off_by_1 = true
+            vim.g.molten_auto_image_popup = false
+            vim.g.molten_output_win_zindex = 50
         end,
-    },
-    {
-        'willothy/wezterm.nvim',
-        config = true
     }
 }
