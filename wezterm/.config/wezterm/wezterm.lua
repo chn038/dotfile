d187106200662ebe070fec667a0048aeb70e9082
chn038 <chenhero940830@gmail.com>
2025-10-10 18:55:56 +0800

    change theme to gruvbux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'kanagawabones'
+config.color_scheme = 'Gruvbox Dark (Gogh)'
 
 config.font = wezterm.font 'Terminess Nerd Font Mono'
 config.font_size = 14
