026930f7af7658ce05ff0bdfcd7729f9dfea35ed
chn038 <chenhero940830@gmail.com>
2025-10-27 20:44:13 +0800

    add rime setting

--- .setup.sh
+++ .setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 stow -v -R *
+mkdir $HOME/.ssh
 cp ssh/.ssh/* $HOME/.ssh
 
 echo "Decrypting id_rsa"
 ansible-vault decrypt $HOME/.ssh/id_rsa
 echo "Decrypting id_rsa_1"
 ansible-vault decrypt $HOME/.ssh/id_rsa_1
+
+echo "Copying rime setting file"
+cp .default.custom.yaml $HOME/.local/share/fcitx5/rime/default.custom.yaml
