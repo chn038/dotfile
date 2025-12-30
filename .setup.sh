6fa2ac6e142438148a2f7b498c254861015fe444
chn038 <chenhero940830@gmail.com>
2025-12-31 03:01:32 +0800

    change to rosepine

--- .setup.sh
+++ .setup.sh
@@ -7,5 +7,3 @@ echo "Decrypting id_rsa"
 ansible-vault decrypt $HOME/.ssh/id_rsa
 echo "Decrypting id_rsa_1"
 ansible-vault decrypt $HOME/.ssh/id_rsa_1
-echo "Copying emoji"
-wget https://raw.githubusercontent.com/rainlime/fcitx-quick-phrase-emoji/master/QuickPhrase.mb && cat ./QuickPhrase.mb >> ~/.config/fcitx/data/QuickPhrase.mb && rm ./QuickPhrase.mb
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,10 +2,10 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.color_scheme = 'Rosé Pine (Gogh)'
 
-config.font = wezterm.font 'Terminess Nerd Font Mono'
-config.font_size = 14
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font_size = 12
 config.use_ime = true
 config.enable_tab_bar = false
 config.enable_wayland = false
