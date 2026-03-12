bf58a75e54ccf2fa915a843541de43bab87c9714
chn038 <chenhero940830@gmail.com>
2026-03-12 22:16:39 +0800

    add gpg keys and change ignore file (stow should be working but I don't know why it doesn't)

--- .setup.sh
+++ .setup.sh
@@ -10,3 +10,20 @@ ansible-vault decrypt $HOME/.ssh/id_rsa_1
 echo "Copying secret and decrypting"
 cp .sec $HOME/.sec
 ansible-vault decrypt $HOME/.sec
+
+echo "Importing gpg keypairs"
+cp gpg/.prv.key $HOME/.prv.key
+cp gpg/.pub.key $HOME/.pub.key
+echo "Decrypting .prv.key"
+ansible-vault decrypt $HOME/.prv.key
+echo "Decrypting .pub.key"
+ansible-vault decrypt $HOME/.pub.key
+echo "Importing keys..."
+gpg --import $HOME/.prv.key
+gpg --import $HOME/.pub.key
+echo "Checking gpg..."
+gpg --list-keys
+echo "Deleting copied keys..."
+rm $HOME/.prv.key
+rm $HOME/.pub.key
+echo "Finished!"
