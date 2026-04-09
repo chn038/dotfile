3c4ac105030003c2f5c9ec6dd471f7e4c9d484a1
chn038 <chenhero940830@gmail.com>
2026-04-09 20:08:20 +0800

    remove unicode charactor

--- nvimconf/.config/nvim/init.lua
+++ nvimconf/.config/nvim/init.lua
@@ -26,6 +26,7 @@ vim.pack.add({
     },
     { src = "https://github.com/rafamadriz/friendly-snippets" },
     { src = "https://github.com/nvim-lualine/lualine.nvim" },
+    { src = "https://github.com/ThePrimeagen/vim-be-good" },
 })
 
 -- hooks
@@ -120,8 +121,8 @@ require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'nord',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         disabled_filetypes = {
             statusline = {},
             winbar = {},
