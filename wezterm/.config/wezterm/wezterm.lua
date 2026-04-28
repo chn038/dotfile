c7d62812c1471de24292edeccd9b99590eb05d00
chn038 <chenhero940830@gmail.com>
2026-04-29 03:37:39 +0800

    change wezterm theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,13 +2,13 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Nord (Gogh)'
+config.color_scheme = 'rose-pine'
 
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.font_size = 10
 config.use_ime = true
 config.enable_tab_bar = false
-config.enable_wayland = true
+config.enable_wayland = false
 
 config.scrollback_lines = 10000
 
