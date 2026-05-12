298ff8d87cc33c44f745abaf96e989ea1a7c0ef5
chn038 <chenhero940830@gmail.com>
2026-05-12 15:27:34 +0800

    change wezterm theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'rose-pine'
+config.color_scheme = 'Catppuccin Mocha'
 
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.font_size = 10
