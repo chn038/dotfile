2a1384fc5707e883ffa692770b495ad9d80f2dbf
chn038 <chenhero940830@gmail.com>
2026-07-29 07:08:22 +0800

    change theme to nord and use vim.pack setting

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'nord'
 
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.font_size = 10
